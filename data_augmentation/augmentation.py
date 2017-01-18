e0b0bf5c0e59ccd340c5ef8213e86b1a93d158ef
Brok Bucholtz <brok.bucholtz@gmail.com>
2017-01-17 19:27:32 -0800

    Add augmentation for scaling

--- data_augmentation/augmentation.py
+++ data_augmentation/augmentation.py
@@ -1,10 +1,11 @@
 from glob import glob
-from random import randint
+from random import randint, choice
 
 from os.path import basename, exists
 
 from os import makedirs
 from skimage import io
+from skimage.transform import rescale
 from skimage.transform import rotate
 from tqdm import tqdm
 import numpy as np
@@ -15,21 +16,34 @@ def rand_rotate(image):
     return rotate(image, angle, preserve_range=True).astype(np.uint8)
 
 
+def rand_scale(image):
+    scale = choice(np.arange(0.5, 0.9, 0.1))
+    return rescale(image, scale, preserve_range=True).astype(np.uint8)
 
 
-def run():
-    rotation_folder = './data/augmentation/rotation/'
+def augment(method, save_dir):
     train_filepaths = list(glob('./data/train/*/*.jpg'))
     labels = [path[13:-14] for path in train_filepaths]
 
     # Create label directories if they don't exist
     for label in labels:
-        if not exists(rotation_folder + label):
-            makedirs(rotation_folder + label)
+        if not exists(save_dir + label):
+            makedirs(save_dir + label)
 
     for file_path, label in tqdm(list(zip(train_filepaths, labels))):
-        augmented_image = rand_rotate(io.imread(file_path))
-        io.imsave(rotation_folder + label + '/' + basename(file_path), augmented_image)
+        augmented_image = method(io.imread(file_path))
+        io.imsave(save_dir + label + '/' + basename(file_path), augmented_image)
+
+
+def run():
+    augments = {
+        'rotate': (rand_rotate, './data/augmentation/rotation/'),
+        'scale': (rand_scale, './data/augmentation/scale/')
+    }
+
+    for name, arguments in augments.items():
+        print('Augmenting {} images:'.format(name))
+        augment(*arguments)
 
 
 if __name__ == '__main__':
--- data_augmentation/combine.py
+++ data_augmentation/combine.py
@@ -6,7 +6,11 @@ from tqdm import tqdm
 
 def run():
     combined_dir = './data/combine/'
-    rotated_filepaths = list(glob('./data/augmentation/rotation/*/*.jpg'))
+
+    aug_file_paths = {
+        'rotated': list(glob('./data/augmentation/rotation/*/*.jpg')),
+        'scaled': list(glob('./data/augmentation/scale/*/*.jpg'))
+    }
 
     # Clear previous combine datasets
     if exists(combined_dir):
@@ -14,9 +18,11 @@ def run():
 
     copytree('./data/train/', combined_dir)
 
-    for rotated_file in tqdm(rotated_filepaths):
-        label = rotated_file[29:-14]
-        copyfile(rotated_file, combined_dir + label + '/' + 'rotated_' + basename(rotated_file))
+    for name, file_paths in aug_file_paths.items():
+        print('Combining {} images'.format(name))
+        for rotated_file in tqdm(file_paths):
+            label = rotated_file[29:-14]
+            copyfile(rotated_file, combined_dir + label + '/' + name + '_' + basename(rotated_file))
 
 if __name__ == '__main__':
     run()
