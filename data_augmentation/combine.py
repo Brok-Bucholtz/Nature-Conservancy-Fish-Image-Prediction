76cee9ba022187736007ad719ca05d446a3c93d3
Brok Bucholtz <brok.bucholtz@gmail.com>
2017-01-17 20:17:24 -0800

    Fix finding label to work with scaled file paths

--- data_augmentation/combine.py
+++ data_augmentation/combine.py
@@ -1,15 +1,14 @@
 from glob import glob
 from shutil import copytree, rmtree, copyfile
-from os.path import exists, basename
+from os.path import exists, basename, dirname
 from tqdm import tqdm
 
 
 def run():
     combined_dir = './data/combine/'
-
-    aug_file_paths = {
-        'rotated': list(glob('./data/augmentation/rotation/*/*.jpg')),
-        'scaled': list(glob('./data/augmentation/scale/*/*.jpg'))
+    augment_data = {
+        'rotated': glob('./data/augmentation/rotation/*/*.jpg'),
+        'scaled': glob('./data/augmentation/scale/*/*.jpg')
     }
 
     # Clear previous combine datasets
@@ -18,11 +17,11 @@ def run():
 
     copytree('./data/train/', combined_dir)
 
-    for name, file_paths in aug_file_paths.items():
+    for name, file_paths in augment_data.items():
         print('Combining {} images'.format(name))
-        for rotated_file in tqdm(file_paths):
-            label = rotated_file[29:-14]
-            copyfile(rotated_file, combined_dir + label + '/' + name + '_' + basename(rotated_file))
+        for augment_file in tqdm(list(file_paths)):
+            label = dirname(augment_file)[len(dirname(dirname(augment_file)))+1:]
+            copyfile(augment_file, combined_dir + label + '/' + name + '_' + basename(augment_file))
 
 if __name__ == '__main__':
     run()
